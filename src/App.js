c8217b423fc27c4fc1bfe939f97446f5bb00940a
Kevin Schmidt <schmidtk14@gmail.com>
2020-10-13 10:23:56 -0400

    Removed signin, signup, profile pages, user auth, firebase from profile repository
    -because the firebase backend is no longer available for this project
     I decided to remove the pages that use it
    -for portfolio purposes this is now just a front end with no user support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import './App.css';
 import Layout from './Layout/Layout'
-import Registration from './Auth/Registration'
-import SignIn from './SignIn/SignIn'
-import SignUp from './SignUp/SignUp'
-import ProfilePage from './ProfilePage/ProfilePage'
-import PasswordReset from './PasswordReset/PasswordReset'
 import {BrowserRouter, Switch, Route} from "react-router-dom"
 import UserProvider from './UserProvider/UserProvider'
 
@@ -15,13 +10,6 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path = {"/"} component = {Layout} />
-          <Route exact path = {"/auth"} component = {Registration} />
-          <Route exact path = {"/signin"} component = {SignIn} />
-          <Route exact path = {"/signup"} component = {SignUp} />
-          <Route exact path = {"/passwordreset"} component = {PasswordReset} />
-
-          <Route exact path = {"/profile"} component = {ProfilePage} />
-
         </Switch>
       </BrowserRouter>
     </UserProvider>
--- src/UserProvider/UserProvider.jsx
+++ src/UserProvider/UserProvider.jsx
@@ -1,5 +1,4 @@
 import React, { Component, createContext } from "react";
-import { auth } from "../firebase";
 
 export const UserContext = createContext({ user: null });
 class UserProvider extends Component {
@@ -7,11 +6,8 @@ class UserProvider extends Component {
     user: null
   };
 
-  componentDidMount = () => {
-    auth.onAuthStateChanged(userAuth => {
-      this.setState({ user: userAuth});
-    });
-  };
+
+  
   render() {
     return (
       <UserContext.Provider value={this.state.user}>
